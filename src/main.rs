4679cced26e8159bb462f152ccc12e909660ea24
Jurriaan Barkey Wolf <info@barkeywolf.consulting>
2022-11-25 22:21:02 +0100

    Endpoint error handling.

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,91 @@ mod generator;
 
 use actix_files as fs;
 use actix_web::{
-    http::header::ContentType, middleware, web, App, HttpResponse, HttpServer, Responder,
+    error,
+    http::{header::ContentType, StatusCode},
+    middleware, web, App, HttpResponse, HttpServer,
 };
+use derive_more::{Display, Error};
 use env_logger::Env;
 use generator::DeepZoomGenerator;
 use image::ImageOutputFormat;
+use log::error;
 use std::{collections::HashMap, path::Path};
 
-async fn tile_endpoint(
+#[derive(Debug, Display, Error)]
+enum DZIRetrievalError {
+    #[display(fmt = "An internal error occurred.")]
+    InternalError,
+
+    #[display(fmt = "Tile request invalid.")]
+    TileRequestInvalid,
+
+    #[display(fmt = "Could not find slide.")]
+    SlideNotFound,
+}
+
+impl error::ResponseError for DZIRetrievalError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::plaintext())
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            DZIRetrievalError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+            DZIRetrievalError::TileRequestInvalid => StatusCode::BAD_REQUEST,
+            DZIRetrievalError::SlideNotFound => StatusCode::NOT_FOUND,
+        }
+    }
+}
+
+async fn get_tile(
     viewers: web::Data<HashMap<String, DeepZoomGenerator>>,
     path: web::Path<(String, u64, u64, u64)>,
-) -> HttpResponse {
+) -> Result<HttpResponse, DZIRetrievalError> {
     let (slide, level, col, row) = path.into_inner();
-
-    // TODO: ensure errors are presented in the frontend.
     let gen = viewers.get(&slide).expect("slide not found");
-    let tile = gen.get_tile(level, col, row).unwrap();
+    let tile = match gen.get_tile(level, col, row) {
+        Ok(tile) => tile,
+        Err(err) => {
+            error!("Could not retrieve tile: {:?}", err);
+            return Err(DZIRetrievalError::TileRequestInvalid);
+        }
+    };
 
     let mut buffer = Vec::new();
+    match tile.write_to(&mut buffer, ImageOutputFormat::Jpeg(80)) {
+        Ok(()) => (),
+        Err(err) => {
+            error!("Jpeg conversion failed: {:?}", err);
+            return Err(DZIRetrievalError::InternalError);
+        }
+    };
 
-    // TODO: evaluate performance of jpg quality
-    tile.write_to(&mut buffer, ImageOutputFormat::Jpeg(80))
-        .unwrap();
-    HttpResponse::Ok()
+    Ok(HttpResponse::Ok()
         .content_type(ContentType::jpeg())
         // TODO: caching is very aggressive and not private. Ensure URL is unique.
         .insert_header(("Cache-Control", "public, max-age=604800, immutable"))
-        .body(buffer)
+        .body(buffer))
 }
 
-async fn dzi(
+async fn get_dzi(
     viewers: web::Data<HashMap<String, DeepZoomGenerator>>,
     path: web::Path<String>,
-) -> impl Responder {
+) -> Result<HttpResponse, DZIRetrievalError> {
     let slide = path.into_inner();
-    let gen = viewers.get(slide.as_str()).expect("slide not found");
-    HttpResponse::Ok()
+    let gen = match viewers.get(slide.as_str()) {
+        Some(tile) => tile,
+        None => {
+            error!("Could not find slide: {}", slide);
+            return Err(DZIRetrievalError::SlideNotFound);
+        }
+    };
+    Ok(HttpResponse::Ok()
         // TODO: caching is very aggressive and not private. Ensure URL is unique.
         .insert_header(("Cache-Control", "public, max-age=604800, immutable"))
-        .body(gen.get_dzi())
+        .body(gen.get_dzi()))
 }
 
 #[tokio::main]
@@ -59,10 +105,10 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .wrap(middleware::DefaultHeaders::new().add(("Access-Control-Allow-Origin", "*")))
             .app_data(state)
-            .route("/{slide}.dzi", web::get().to(dzi))
+            .route("/{slide}.dzi", web::get().to(get_dzi))
             .route(
                 "/{slide}_files/{level}/{col}_{row}.jpg",
-                web::get().to(tile_endpoint),
+                web::get().to(get_tile),
             )
             .service(fs::Files::new("/static", "./public/static").show_files_listing())
             .service(fs::Files::new("/", "./public/index.html").show_files_listing())
